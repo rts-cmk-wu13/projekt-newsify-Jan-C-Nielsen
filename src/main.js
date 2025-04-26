d9a98ab710672c35160c127cc35518c6b777101e
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-04-26 21:11:36 +0200

    onboarding

--- src/main.js
+++ src/main.js
@@ -6,16 +6,44 @@ import onboarding1 from '../assets/Onboarding_1.png'
 import onboarding2 from '../assets/Onboarding_2.png'
 import onboarding3 from '../assets/Onboarding_3.png'
 import frame1 from '../assets/Frame_1.png'
+import frame2 from '../assets/Frame_2.png'
+import frame3 from '../assets/Frame_3.png'
 let apikey = `MibrQ7dgs0zI5KvQbJWplGJOvpjveLSh`
 let apiUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=`
-
-
-document.querySelector('#app').innerHTML = `
+let i = 0
+let layout = [`
 <img src="${onboarding1}">
 <h1>Stay Connected,<br>
 Everywhere, Anytime</h1>
 <p class="onboarding">Welcome to Newsify, your ultimate destination for breaking news, exclusive stories, and tailored content.</p>
 <div><img src="${frame1}"></div>
-<div class="buttonrow"><button>Skip</button><button class="continue">Continue</button></div>
+<div class="buttonrow"><button class="skip">Skip</button><button class="continue">Continue</button></div>
+`,
+`<img src="${onboarding2}">
+<h1>Become a Savvy<br>Global Citizen.</h1>
+<p class="onboarding">Discover tailored news that aligns with your interests and preferences. Your personalized news journey awaits!</p>
+<div><img src="${frame2}"></div>
+<div class="buttonrow"><button class="skip">Skip</button><button class="continue">Continue</button></div>`,
+`<img src="${onboarding3}">
+<h1>Enhance your News<br>
+Journey Now!</h1>
+<p class="onboarding">Be part of our dynamic community and contribute your insights and participate in enriching conversations.</p>
+<div><img src="${frame3}"></div>
+<div class="buttonrow"><button class="skip">Skip</button><button class="continue">Continue</button></div>`
+]
 
-`
+document.querySelector('#app').innerHTML = layout[i]
+
+document.querySelector(".skip").addEventListener("click", skipFunc)
+document.querySelector(".continue").addEventListener("click",continueFunc)
+
+
+function skipFunc(){
+    window.close()
+}
+
+function continueFunc(){
+    document.querySelector('#app').innerHTML = layout[++i]
+    document.querySelector(".skip").addEventListener("click", () => window.close())
+    document.querySelector(".continue").addEventListener("click",continueFunc)
+}
