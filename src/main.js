a8cbca67609c43c4ffd5295e270da5d6cd16bfde
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-04-28 12:14:28 +0200

    pseudo login

--- src/main.js
+++ src/main.js
@@ -8,9 +8,14 @@ import onboarding3 from '../assets/Onboarding_3.png'
 import frame1 from '../assets/Frame_1.png'
 import frame2 from '../assets/Frame_2.png'
 import frame3 from '../assets/Frame_3.png'
+import big_logo from '../assets/Big_newsify_logo.png'
+import line from '../assets/Line.png'
+
 let apikey = `MibrQ7dgs0zI5KvQbJWplGJOvpjveLSh`
 let apiUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=`
+
 let i = 0
+
 let layout = [`
 <img src="${onboarding1}">
 <h1>Stay Connected,<br>
@@ -29,21 +34,39 @@ Everywhere, Anytime</h1>
 Journey Now!</h1>
 <p class="onboarding">Be part of our dynamic community and contribute your insights and participate in enriching conversations.</p>
 <div><img src="${frame3}"></div>
-<div class="buttonrow"><button class="skip">Skip</button><button class="continue">Continue</button></div>`
+<div class="buttonrow"><button class="skip">Skip</button><button class="continue">Continue</button></div>`,
+`<img src="${big_logo}">
+<h1>Newsify</h1>
+<p class="onboarding">Welcome! Let’s dive into your account!</p>
+<button class="login continue">Continue with Facebook</button>
+<button class="login continue">Continue with Google</button>
+<div class="or"><img src="${line}"><span>or</span><img src="${line}"></div>
+<button class="login continue">Sign in with password</button></div>
+<p>Don’t have an account?  Sign up</p>
+`
 ]
 
-document.querySelector('#app').innerHTML = layout[i]
+document.querySelector('#app').innerHTML = layout[i++]
 
-document.querySelector(".skip").addEventListener("click", skipFunc)
-document.querySelector(".continue").addEventListener("click",continueFunc)
+let skip = document.querySelector(".skip")
+if (skip) skip.addEventListener("click", () => window.close())
+document.querySelectorAll(".continue").forEach(x => x.addEventListener("click", continueFunc))
 
-
-function skipFunc(){
+function skipFunc() {
     window.close()
 }
 
-function continueFunc(){
-    document.querySelector('#app').innerHTML = layout[++i]
-    document.querySelector(".skip").addEventListener("click", () => window.close())
-    document.querySelector(".continue").addEventListener("click",continueFunc)
+function continueFunc() {
+    
+    if (i < layout.length) {
+        console.log(i)
+        document.querySelector('#app').innerHTML = layout[i]
+        let skip = document.querySelector(".skip")
+        if (skip) skip.addEventListener("click", () => window.close())
+        document.querySelectorAll(".continue").forEach(x => x.addEventListener("click", continueFunc))
+        i++
+    }
+    else {
+        window.location.href = "./home/index.html";
+    }
 }
